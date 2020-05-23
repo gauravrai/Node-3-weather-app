04247595a0e6420dfdae75da0583058cb85d40d1
gauravrai <rai.grv@gmail.com>
2020-05-23 20:57:32 +0530

    Added extra features to the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request = require('request')
 const forecast = (latitude, longitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=5106cd31fffa760bc05c54610ac5361f&query=${latitude},${longitude}&units=f`
+    const url = `http://api.weatherstack.com/current?access_key=5106cd31fffa760bc05c54610ac5361f&query=${latitude},${longitude}`
 
     request({url, json: true}, (error, {body}) => {
         if(error){
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(error)
         }
         else{
-            callback(null, `${body.current.weather_descriptions[0]} It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out`)
+            callback(null, `${body.current.weather_descriptions[0]} It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Air pressure is ${body.current.pressure}. `)
         }
     })
 }
